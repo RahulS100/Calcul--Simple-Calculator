752f4ec0c32b38716f58349c36c113f0ec0182b2
rahul <rahulsaharan554@gmail.com>
2021-10-06 10:08:32 +0530

    added the cut button and some minor bug fixed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const keys = document.querySelectorAll('.keys');
 const clear = document.getElementById('clear');
 const calcul = document.getElementById('calcul');
 const out_dom = document.getElementById('out');
+const cut_btn = document.getElementById('cut_btn');
 
 //variables
 let str_exp = '';
@@ -13,25 +14,41 @@ let out = 0;
 //digit buttons handling events
 keys.forEach((element, index)=> {
    element.addEventListener('click', ()=>{
-    let check = keys[index].innerText;
 
-       if(!keys[index].classList.contains('rem')) {
-    str_exp += keys[index].innerText;
-    input_num.value = str_exp;
+    if(!keys[index].classList.contains('rem')) {
+        str_exp += keys[index].innerText;
+         input_num.value = str_exp;
        }
    }); ///click event listener end
 });
 ///digit handling event ends here
 
-//clear the input
+
+////////clear the input
 clear.addEventListener('click', ()=> {
     input_num.value = '';
     out_dom.innerText = '';
     str_exp = '';
 });
 
-//calculate
+//////////calculate
 calcul.addEventListener('click', ()=> {
     out = eval(str_exp);
     out_dom.innerText = out;
+    input_num.value = out;
+    str_exp = String(out);
+    console.log(str_exp);
 });
+
+
+///////cut button
+cut_btn.addEventListener('click', ()=>{
+    str_exp =  str_exp.slice(0, -1);
+    input_num.value = str_exp;
+});
+
+
+///////refresh clear the input_fields//////////////
+window.onload = ()=> {
+    input_num.value = '';
+}
